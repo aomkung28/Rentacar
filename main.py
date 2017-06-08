e6ed5c9323b81fae5b1eb1d1eef939ba993853c2
SNiPErMAXxi <saber@contentsme.com.au>
2017-06-08 18:55:45 +1000

    Add Booking Route

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from flask import *
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def dashboard():
     return render_template('dashboard.html')
+
+@app.route("/booking")
+def booking():
+    return render_template('booking.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
