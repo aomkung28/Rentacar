4234df67a7111ce33afe9f181dd7b9e3bdcf3041
SNiPErMAXxi <saber@contentsme.com.au>
2017-06-09 18:39:18 +1000

    Combine Sidebar Menu

--- main.py
+++ main.py
@@ -11,7 +11,23 @@ def dashboard():
 def booking():
     return render_template('booking.html')
 
+@app.route("/car")
+def car():
+    return render_template('car.html')
 
+
+@app.route("/profile")
+def profile():
+    return render_template('profile.html')
+
+@app.route("/report")
+def report():
+    return render_template('report.html')
+
+@app.route("/invoice")
+def invoice():
+    return render_template('invoice.html')
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
